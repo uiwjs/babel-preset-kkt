475e7925818221f3ebb09ee43cc3accbb88ee274
jaywcjlove <398188662@qq.com>
2018-11-26 22:14:16 +0800

    Init project.

---
+++ README.md
@@ -0,0 +1,21 @@
+babel-preset-kkt
+---
+
+This package includes the [Babel](https://babeljs.io) preset used by kkt.
+
+
+## Usage Outside of kkt
+
+If you want to use this Babel preset in a project not built with `kkt`, you can install it with following steps.
+
+First, [install Babel](https://babeljs.io/docs/setup/).
+
+Then create a file named `.babelrc` with following contents in the root folder of your project:
+
+```js
+{
+  "presets": ["kkt"]
+}
+```
+
+This preset uses the `useBuiltIns` option with [transform-object-rest-spread](http://babeljs.io/docs/plugins/transform-object-rest-spread/), which assumes that `Object.assign` is available or polyfilled.
---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var env = process.env.BABEL_ENV || process.env.NODE_ENV;
+if (env !== 'development' && env !== 'test' && env !== 'production') {
+  throw new Error(
+    'Using `babel-preset-kkt` requires that you specify `NODE_ENV` or ' +
+    '`BABEL_ENV` environment variables. Valid values are "development", ' +
+    '"test", and "production". Instead, received: ' +
+    JSON.stringify(env) +
+    '.'
+  );
+}
+
+var preset = {
+  presets: [
+    [
+      require.resolve('@babel/preset-env'),
+      {
+        targets: {
+          browsers: [
+            'last 2 versions',
+            'ie >= 10',
+          ],
+        },
+      },
+    ],
+    require.resolve('@babel/preset-react'),
+  ],
+  plugins: [
+    require.resolve('@babel/plugin-syntax-dynamic-import'),
+    require.resolve('@babel/plugin-proposal-class-properties'),
+    require.resolve('@babel/plugin-transform-async-to-generator'),
+    [
+      require.resolve('@babel/plugin-proposal-object-rest-spread'),
+      {
+        loose: true,
+        useBuiltIns: true,
+      },
+    ],
+    [
+      require.resolve('@babel/plugin-transform-runtime'),
+      {
+        corejs: false,
+        helpers: true,
+        regenerator: true,
+        useESModules: false,
+      },
+    ],
+  ],
+}
+
+if (env === 'production') {
+  preset.plugins.push.apply(preset.plugins, [
+    require.resolve('babel-plugin-transform-react-remove-prop-types'),
+  ]);
+}
+
+module.exports = preset;
